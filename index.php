30d8ee9a531cf25a5aaeb5d68e4619f61dda8190
slavadirta <slavadirta@gmail.com>
2025-11-10 12:58:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php date_default_timezone_set('Asia/Jakarta'); if(isset($_POST['pesan'])){ $tanggal = date("d-M-Y (H:i)"); $pesan = $_POST['pesan']; $fp = fopen('pesan.txt', 'a'); fwrite($fp, '
+<div class="card-pesan"><p><span>Pesan :</span><br />'.$pesan.'</p><h6>'.$tanggal.'</h6></div>'); echo json_encode(array("statusCode"=>200)); fclose($fp); die; } $fp_add = fopen('pesan.txt', 'a'); $fp_read = fopen('pesan.txt', 'r'); $ipp = preg_replace("/\s+/", "", fgets($fp_read)); if($ipp!=""){ $sbs = "false"; $tmpl = $ipp==getUserIpAddr() ? 'false' : 'true'; } else { $sbs = "true"; $tmpl = 'false'; if(isset($_GET["subscribe"])){ fwrite($fp_add, getUserIpAddr()); echo json_encode(array("statusCode"=>200)); die; } } fclose($fp_read); fclose($fp_add); function getUserIpAddr(){ if(!empty($_SERVER['HTTP_CLIENT_IP'])){ $ip = $_SERVER['HTTP_CLIENT_IP']; } else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){ $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; }else{ $ip = $_SERVER['REMOTE_ADDR']; } return $ip; } ?><!DOCTYPE html> <html> <head> <meta charset="utf-8" /> <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> <title>I Love You</title> <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.4/dist/sweetalert2.all.min.js"></script> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <script><?="var tampil=".$tmpl."; var sbs = ".$sbs.";"?></script> <script src="https://cdn.jsdelivr.net/gh/slavadirta/hbd@main/hbd.js"></script> <link rel="stylesheet" href="https://dekatutorial.github.io/slide/dekatutorialstyle.css" /> </head> <body> <?php if(isset($_GET['pesan'])){ ?> <div class="bg"></div> <div class="content"> <?php $fp = fopen('pesan.txt', 'r'); fgets($fp); while(!feof($fp)){ echo "".fgets($fp); } fclose($fp); ?> </div> </body> </html> <?php die; } ?> <div class="open"> <div class="left"></div> <div class="right"></div> <div class="content"> <div class="lock"> <div class="top"></div> <div class="bottom"></div> </div> </div> <div class="text"> <p>Klik Gemboknya</p> </div> </div> <div id="slider" class="slider"> <div class="wrapper"> <div id="items" class="items">
+
+  <div>
+    <img src="stiker1.gif" />
+    <p>hallo mei sayang<p>geser ke samping ya</p>
+  </div>
+  <div>
+    <img src="hbd1.gif" />
+    <p>selamat ulang tahun Mei ku sayang</p>
+  </div>
+  <div>
+    <img src="hbd2.gif" />
+    <p>ciee yang bertambah umur, horee horee horeeee</p>
+  </div>
+  <div>
+    <img src="hbd3.gif" />
+    <p>semoga tercapai segala keinginan kamu ya sayang</p>
+  </div>
+  <div>
+    <img src="stiker2.gif" />
+    <p>maaf ya sayang aku cuma bisa ngasih ini 🥺</p>
+  </div>
+  <div>
+    <img src="Mei.jpeg" />
+    <p>Cantiknya Akuuuuu...<p>I Love You</p></p>
+  </div>
+
+<div></div></div></div></div><script>
+
+musik = "hbd.mp3";
+idtelegram = "@";
+
+htmlscript(musik, idtelegram);
+</script></body></html>
